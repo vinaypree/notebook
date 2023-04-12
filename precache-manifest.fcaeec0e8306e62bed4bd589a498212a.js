9585428c2e1574963f653b3787c75b801bcc9d61
vinaypree <vinaypreetsingh90@gmail.com>
2023-04-12 10:06:50 +0530

    Updates

---
+++ precache-manifest.fcaeec0e8306e62bed4bd589a498212a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6ec53daff5a6beef7a5",
+    "url": "/notebook/static/css/main.3de2c9fc.chunk.css"
+  },
+  {
+    "revision": "d6ec53daff5a6beef7a5",
+    "url": "/notebook/static/js/main.d6ec53da.chunk.js"
+  },
+  {
+    "revision": "234cc1d91d6429fe0e90",
+    "url": "/notebook/static/js/1.234cc1d9.chunk.js"
+  },
+  {
+    "revision": "7a4254bda4509c4b35ec",
+    "url": "/notebook/static/js/runtime~main.7a4254bd.js"
+  },
+  {
+    "revision": "708b3d3463564329357643c3b9756e87",
+    "url": "/notebook/index.html"
+  }
+];
